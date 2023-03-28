e7d864753785c70f0327a00e527cee655795c333
joy1192 <1261820+joy1192@users.noreply.github.com>
2023-03-28 16:33:18 +0000

    openapiはlocalでのみホストするよう修正

--- nestjs/backend/src/main.ts
+++ nestjs/backend/src/main.ts
@@ -3,19 +3,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
-  // OpenAPI用の設定を追加
-  const options = new DocumentBuilder().
-    setTitle('NestJS Sample API').
-    setDescription('NestJS Sample API Description').
-    setVersion('1.0').
-    addTag('NestJS Sample API').
-    build();
-
   const app = await NestFactory.create(AppModule);
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
 
+  if (process.env.NODE_ENV === 'local') {
+    // OpenAPI用の設定を追加
+    const options = new DocumentBuilder().
+      setTitle('NestJS Sample API').
+      setDescription('NestJS Sample API Description').
+      setVersion('1.0').
+      addTag('NestJS Sample API').
+      build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
   await app.listen(80);
 }
 bootstrap();
